7bc588e22226ce0d67ee5c1d45d7d062eb4cef05
Abraham Galue <abrahamgalue512@gmail.com>
2023-11-07 15:42:10 -0400

    feat: add lastname input

--- src/pages/complices.py
+++ src/pages/complices.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 
+
 class FormularioComplices(tk.Tk):
     """Formulario de registro de los complices"""
 
@@ -26,13 +27,18 @@ class FormularioComplices(tk.Tk):
         self.name_entry = tk.Entry(width=20)
         self.name_entry.grid(row=2, column=1)
 
+        lastname_label = tk.Label(text="Apellido:", justify=tk.LEFT)
+        lastname_label.grid(row=3, column=0, sticky=tk.W)
+        self.lastname_entry = tk.Entry(width=20)
+        self.lastname_entry.grid(row=3, column=1)
+
         dob_label = tk.Label(text="Fecha de nacimiento:")
-        dob_label.grid(row=3, column=0, sticky=tk.W)
+        dob_label.grid(row=4, column=0, sticky=tk.W)
         self.dob_entry = tk.Entry(width=20)
-        self.dob_entry.grid(row=3, column=1)
+        self.dob_entry.grid(row=4, column=1)
 
         sex_label = tk.Label(text="Sexo:")
-        sex_label.grid(row=4, column=0, sticky=tk.W)
+        sex_label.grid(row=5, column=0, sticky=tk.W)
 
         self.sex_entry = ttk.Combobox(
             state="readonly",
@@ -40,28 +46,17 @@ class FormularioComplices(tk.Tk):
             values=["M", "F"]
         )
         self.sex_entry.current(0)
-        self.sex_entry.grid(row=4, column=1)
+        self.sex_entry.grid(row=5, column=1)
 
         phone_label = tk.Label(text="Número de teléfono:")
-        phone_label.grid(row=5, column=0, sticky=tk.W)
+        phone_label.grid(row=6, column=0, sticky=tk.W)
         self.phone_entry = tk.Entry(width=20)
-        self.phone_entry.grid(row=5, column=1)
+        self.phone_entry.grid(row=6, column=1)
 
         address_label = tk.Label(text="Dirección:")
-        address_label.grid(row=6, column=0, sticky=tk.W)
+        address_label.grid(row=7, column=0, sticky=tk.W)
         self.address_entry = tk.Entry(width=20)
-        self.address_entry.grid(row=6, column=1)
-
-        civil_status_label = tk.Label(text="Estado civil:")
-        civil_status_label.grid(row=7, column=0, sticky=tk.W)
-
-        self.civil_status_entry = ttk.Combobox(
-            state="readonly",
-            width=20,
-            values=["Soltero", "Casado"]
-        )
-        self.civil_status_entry.current(0)
-        self.civil_status_entry.grid(row=7, column=1)
+        self.address_entry.grid(row=7, column=1)
 
         nationality_label = tk.Label(text="Nacionalidad:")
         nationality_label.grid(row=8, column=0, sticky=tk.W)
@@ -97,10 +92,13 @@ class FormularioComplices(tk.Tk):
         patron_nombre = r'^[\S]{1,60}$'
         self.regex_nombre = re.compile(patron_nombre)
 
+        patron_apellido = r'^[\S]{1,60}$'
+        self.regex_apellido = re.compile(patron_apellido)
+
         patron_fecha = r'^([0-9]{4})-((?:0?[1-9]|1[0-2])|(?:0?[1-9]|1[0-2]|2[0-8]))-((?:0?[1-9]|[1-2][0-9]|3[0-1]))$'
         self.regex_fecha = re.compile(patron_fecha)
 
-        patron_telefono = r'^\d{7,10}$'
+        patron_telefono = r"^(\(?\+[\d]{1,3}\)?)\s?([\d]{1,5})\s?([\d][\s\.-]?){6,7}$"
         self.regex_telefono = re.compile(patron_telefono)
 
         patron_direccion = r'^[a-zA-Z0-9_\-.\s]{0,200}$'
@@ -120,6 +118,13 @@ class FormularioComplices(tk.Tk):
         if re.match(self.regex_nombre, nombre) is None:
             messagebox.showwarning(
                 'Nombre inválido', 'El nombre no es válido')
+            return
+
+        apellido = self.lastname_entry.get()
+
+        if re.match(self.regex_apellido, apellido) is None:
+            messagebox.showwarning(
+                'Apellido inválido', 'El apellido no es válido')
             return
 
         fecha = self.dob_entry.get().strip()
@@ -152,11 +157,11 @@ class FormularioComplices(tk.Tk):
 
         criminal_data = {
             "Nombre": self.name_entry.get(),
+            "Apellido": self.lastname_entry.get(),
             "Fecha de nacimiento": self.dob_entry.get(),
             "Sexo": self.sex_entry.get(),
             "Número de teléfono": self.phone_entry.get(),
             "Dirección": self.address_entry.get(),
-            "Estado civil": self.civil_status_entry.get(),
             "Nacionalidad": self.nationality_entry.get(),
             "Alias": self.alias_entry.get()
         }
@@ -169,13 +174,12 @@ class FormularioComplices(tk.Tk):
 
     def clean(self):
         """Limpiar los campos del formulario"""
-        self.dni_entry.delete(0, tk.END)
         self.name_entry.delete(0, tk.END)
+        self.lastname_entry.delete(0, tk.END)
         self.dob_entry.delete(0, tk.END)
         self.sex_entry.current(0)
         self.phone_entry.delete(0, tk.END)
         self.address_entry.delete(0, tk.END)
-        self.civil_status_entry.current(0)
         self.nationality_entry.current(0)
         self.alias_entry.delete(0, tk.END)
 
