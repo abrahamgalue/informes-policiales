8625e02b06a0d883b896b2e7d120851934bba590
Ale <ale.arteaga.18.09@gmail.com>
2023-11-09 13:37:43 -0400

    feat: conneccion function added to functions.py for easier access to the mysql password

--- src/pages/util/functions.py
+++ src/pages/util/functions.py
@@ -2,56 +2,10 @@ import datetime
 import mysql.connector
 from mysql.connector import Error
 
-def get_persona(ced):
+def conneccion(pswrd = ''):
     try:
-        connection = mysql.connector.connect(host='localhost',database='proyecto',user='root',password='')
+        connection = mysql.connector.connect(host='localhost',database='proyecto',user='root',password=pswrd)
         if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute(f"SELECT * FROM persona WHERE numero_de_identificacion = %s;",(ced,))
-            implicado = cursor.fetchall()
-            print(implicado)
+            return connection
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
-    return implicado
-#get_persona(112233)
-
-def get_personas():
-    try:
-        connection = mysql.connector.connect(host='localhost',database='proyecto',user='root',password='')
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute("SELECT * FROM persona ORDER BY nombre;")
-            implicados = cursor.fetchall()
-            print(implicados)
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
-        return implicados
-
-def add_persona(values):
-    try:
-        connection = mysql.connector.connect(host='localhost',database='proyecto',user='root',password='')
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute("INSERT INTO persona VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);",values)
-            print('Warnings:',cursor.fetchwarnings())
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.commit()
-            connection.close()
-            print("MySQL connection is closed")        
-#FORMATO PARA INGRESAR DATOS EN PERSONAS
-# datos = (112233,'Elba','Jito',datetime.date(1999,9,9),'Masculino','999-999-9999','No','Hell','El bajito')
-# add_persona(datos)
